bfe13cb8d218c919a10e7e32b2b2e410773d9fe9
Jefferson Rivera <jeff@coink.com>
2019-06-18 09:36:54 -0500

    build

---
+++ build/precache-manifest.f236c9b3b30a50e5fb32baa99865c946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb218eb0e01455ec59cecdf98a7af8ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "804186074d449e0a0740",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "804186074d449e0a0740",
+    "url": "/static/js/main.d3862834.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
